e9025c7319a5055a556f6b1ab4a1f61e5b3df666
- <victorliu5296@outlook.com>
2024-09-25 17:35:17 -0400

    Update replace-delimiters.sh

--- replace-delimiters.sh
+++ replace-delimiters.sh
@@ -11,22 +11,11 @@ replace_delimiters() {
         sub process_math {
             my ($content) = @_;
 
+            # Escape backslashes if not preceded by a backslash
+            $content =~ s/(?<!\\)\\/\\\\/g;
+
             # Escape double backslashes
             $content =~ s/\\\\/\\\\\\\\/g;
-
-            # Only escape \begin and \end if they are not already escaped
-            $content =~ s/(?<!\\\\)\\begin/\\\\begin/g;
-            $content =~ s/(?<!\\\\)\\end/\\\\end/g;
-
-            # Escape underscores if not already escaped
-            $content =~ s/(?<!\\)_/\\_/g;
-
-            # Only escape rendered curly brackets
-            $content =~ s/\\left\\\{/\\left\\\\\{/g;
-            $content =~ s/\\right\\\}/\\right\\\\\}/g;
-
-            # Single-escape commas
-            $content =~ s/\\\\,/\\,/g;
 
             return $content;
         }
